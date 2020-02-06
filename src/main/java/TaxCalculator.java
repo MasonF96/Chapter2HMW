9e319bed460ed8fb2c5e1f77ed94480a2e1879ae
mason <mason@dhcp-172-16-249-164.dyn.pittstate.edu>
2020-02-06 09:16:04 -0600

    Scanner setup

---
+++ src/main/java/TaxCalculator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mason
+ */
+public class TaxCalculator {
+    public static void main(String[] args) {
+        scanner keyboard = new scanner (System.in)
+    }
+}
